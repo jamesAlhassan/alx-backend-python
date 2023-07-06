7a0d2b01421b93f26e2d14e4f222521ff57036b1
James Alhassan <jamesalhassan052@gmail.com>
2023-07-06 15:39:58 +0000

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''type-annotated function floor which takes a float n as 
+'''type-annotated function floor which takes a float n as
 argument and returns the floor of the float.
 '''
 
