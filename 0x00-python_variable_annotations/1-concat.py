44bd506857cb8e6c65fdb4876af56d04191bbf0e
James Alhassan <jamesalhassan052@gmail.com>
2023-07-06 15:31:19 +0000

    Add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+type-annotated function concat that takes a string str1 and
+a string str2 as arguments and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    'concatenates strings: str1 and str2'
+    return str(str1 + ' ' + str2)
