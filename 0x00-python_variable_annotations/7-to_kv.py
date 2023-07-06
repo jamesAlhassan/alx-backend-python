21415c503665b98c89621c0110d4e745d6142fda
James Alhassan <jamesalhassan052@gmail.com>
2023-07-06 19:40:24 +0000

    Add 7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+type-annotated function to_kv that takes a string k and
+an int OR float v as arguments and returns a tuple
+The first element of the tuple is the string k.
+The second element is the square of the int/float v and
+should be annotated as a float.
+'''
+
+
+from typing import Union
+from typing import Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    'returns k and square of v in a tuple'
+    return (k, float(v**2))
